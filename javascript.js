af1f9423c3dfb77b33c158248d902602a2fda229
gabrielmarquesdeassis2008 <gabrielmarquesdeassis2008@gmail.com>
2025-12-05 13:47:32 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,14 @@
-let i=10
-for(i=10;i<=100;i=i+10){
-    document.writeln(i)
+let a=parseInt(prompt('digite um número'))
+b=parseInt(prompt('digite o segundo número'))
+c=parseInt(prompt('digite o terceiro número'))
+ if(a>b && a>c){
+    document.writeln(a)
 }
+else if(b>a && b>c){
+    document.writeln(b)
+}
+else if(c>a && c>b){
+    document.writeln(c)
+}
+alert('Agora direi qual é o maior')
+
